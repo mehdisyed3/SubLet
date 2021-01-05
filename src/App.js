1395c45a343fbf8410644378ff33a1a953cc17e3
Syed Mehdi <mehdisyed3@gmail.com>
2021-01-05 16:37:17 -0500

    HEader.js moved to App component, minor changes in Banner.css, Home.css and SearchDAte.css - Card Component created. and redndering in Home.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
 import Home from './Home/Home';
+import Header from './Header/Header'
 
 
 function App() {
   return (
     <div className="app">
+      <Header />
       <Home />
       
     </div>
