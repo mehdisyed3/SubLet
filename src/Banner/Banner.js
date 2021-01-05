63b39e0c4a31268718a51ff0de5a9ac146eb87e0
Syed Mehdi <mehdisyed3@gmail.com>
2021-01-05 16:14:44 -0500

    SearchDate.js complete

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,19 +1,25 @@
 import React, { useState } from 'react'
 import './Banner.css'
 import {Button } from '@material-ui/core'
+import SearchDate from '../SearchDate/SearchDate'
 
 function Banner() {
   const [showSearch,setShowSearch]=useState(false)
   return (
     <div className='banner'>
       <div className='banner__search'>
+      {showSearch && <SearchDate />}
+      
         <Button 
           className='banner__searchButton'
           variant='outlined'
           onClick={()=> setShowSearch(!showSearch)} >
+
             { showSearch ? 'Hide' : 'Search Dates'}
           
           </Button>
+          
+
       </div>
       <div className='banner__info'>
         <h1>Find a home away from home</h1>
---
+++ src/SearchDate/SearchDate.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import './SearchDate.css'
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRangePicker } from "react-date-range";
+import { Button } from "@material-ui/core";
+import PeopleIcon from "@material-ui/icons/People";
+// import { useHistory } from "react-router-dom";
+
+
+
+function SearchDate() {
+  const [startDate,setStartDate]=useState(new Date())
+  const [endDate,setEndDate ]=useState(new Date())
+
+  const selectionRange = {
+    startDate : startDate,
+    endDate : endDate,
+    key : 'selection'
+  }
+
+  function handleSelect(ranges) {
+    setStartDate(ranges.selection.startDate);
+    setEndDate(ranges.selection.endDate);
+}
+
+    return (
+      <div className='searchDate'>
+        <DateRangePicker ranges={[selectionRange]} onChange={handleSelect} />
+        <h2>Number of guests <PeopleIcon/> </h2>
+        <input type='number' min={0} defaultValue = {2} />
+        <Button >Search</Button>
+      </div>
+    )
+  }
+
+export default SearchDate
