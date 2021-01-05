fd49b9c7fdca8f6a74be6280eb437bff83bbb134
Syed Mehdi <mehdisyed3@gmail.com>
2021-01-05 16:46:27 -0500

    Card.js complete with styling

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import "./Card.css"
 
-function Card({ src, title,description, price }) {
+function Card({ src, title, description, price }) {
   return (
     <div className='card'>
-      <img src={src} alt=''/>
-      <div className='card__'>
-
+      <img src={src} alt='' />
+      <div className='card__info'>
+        <h2>{title}</h2>
+        <h4>{description}</h4>
+        <h3>{price}</h3>
       </div>
     </div>
   )
