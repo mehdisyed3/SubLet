b7753b187a38c317ab300b6315df3c9e42cc2c33
Syed Mehdi <mehdisyed3@gmail.com>
2021-01-06 15:40:27 -0500

    update to SearchPAge

---
+++ src/SearchResult/SearchResult.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './SearchResult.css'
+
+function SearchResult({ img,location,title,description,star,price,total,}) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SearchResult
